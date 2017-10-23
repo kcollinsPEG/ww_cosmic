a5148d5af6a2ca18fc2dfae60c0375cb33171711
kcollinsPEG <32527169+kcollinsPEG@users.noreply.github.com>
2017-10-23 16:49:24 -0500

    Update book-now-email.js

--- routes/book-now-email.js
+++ routes/book-now-email.js
@@ -23,7 +23,7 @@ var bookEmail = (app) => {
     if(typeof recaptcha === undefined || recaptcha === '' || recaptcha === null) {
       return res.json({success: 2});
     }
-    var secretKey = "6Ldj1BsUAAAAAGuUmnKRwHRHe41wkuE20oTk2676";
+    var secretKey = "6LdfJjEUAAAAAF1fImzF9Mskry3BLc5JtOskwAXn";
     var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
 
     request(verificationUrl,function(error,response,body) {
